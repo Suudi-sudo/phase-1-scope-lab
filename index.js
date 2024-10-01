3580d201e9632f5d8e252370e58df1328266b3c7
suudi abdisalan <abdisalansucdi@gmail.com>
2024-10-01 10:46:06 +0300

    done lap

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+var  customerName = 'bob';
+//function OF uppercasename
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase();
+}
+//function to setbestcustomer
+
+var  bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+
+
+//function of overwritebestcustomer
+{ var bestCustomer = 'not bob';
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+ 
+}
+console.log(bestCustomer); // 'not bob'
+overwriteBestCustomer();
+console.log(bestCustomer); // 'maybe bob'
+}
+
+const LeastFavoriteCustomer = 'bob';
+
+//function
+function changeLeastFavoriteCustomer(){
+    
+    LeastFavoriteCustomer = 'maybe not bob';
+    console.log(LeastFavoriteCustomer);
+    
+}
+
